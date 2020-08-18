014e23e80159f68540bd71eb1383a6fc76e7741f
aaronjparsons <aaron.parsons@live.ca>
2020-08-17 20:46:38 -0600

    Initialize project

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { slide } from 'svelte/transition';
+  import Tailwindcss from './Tailwindcss.svelte';
+  import MessageCard from './components/MessageCard.svelte'
+
+  let messages = [
+    {
+      user: 'User 1',
+      content: 'Hey there!'
+    },
+    {
+      user: 'Carlton',
+      content: 'Do the dance!'
+    }
+  ];
+
+  function addMessage() {
+    messages = [{ user: `User ${messages.length + 1}`, content: 'Another message' }, ...messages];
+  }
+</script>
+
+<main>
+  <button on:click="{addMessage}">Add Message</button>
+  {#each messages as message, index (message)}
+    <div transition:slide>
+      <MessageCard message="{message}" />
+    </div>
+  {/each}
+</main>
+<Tailwindcss />
---
+++ src/components/MessageCard.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let message;
+</script>
+
+<div class="rounded-md shadow-md p-4 m-4 bg-gray-200">
+  <p>{message.user}</p>
+  <p>{message.content}</p>
+</div>
